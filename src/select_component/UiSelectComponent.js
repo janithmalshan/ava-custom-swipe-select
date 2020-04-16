e7619454693948f7f008852a3c7c0301f0ef3667
Janith Malshan <janithmalshan@gmail.com>
2020-04-16 11:32:54 +0300

    Readme file

--- src/select_component/UiSelectComponent.js
+++ src/select_component/UiSelectComponent.js
@@ -4,11 +4,14 @@ import {UiSelect} from "./UiSelect";
 export default class UiSelectComponent extends React.Component {
 
     render() {
+
+        //Pass data list from here
         const data =[{"value":"16"},{"value":"24"},{"value":"37"},{"value":"56"},{"value":"84"},{"value":"94"},{"value":"114"}];
         const listItems = data.map((d) => <div className="price-item" key={d.value}>{d.value}</div>);
 
         return (
             <div className="select-wrapper">
+                {/*Pass the values to the component*/}
                 <UiSelect price={listItems}/>
                 <p className="select-wrapper__month">€/Month</p>
             </div>
